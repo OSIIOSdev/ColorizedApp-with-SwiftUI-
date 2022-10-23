eb25e557a37c5e3bfe1134c36080dd7d29acfd18
OSIIOSdev <OSIIOS.dev@gmail.com>
2022-10-23 13:34:22 -0700

    Initial Commit

---
+++ ColorizedApp-with-SwiftUI-/ColorizedApp_with_SwiftUI_App.swift
@@ -0,0 +1,17 @@
+//
+//  ColorizedApp_with_SwiftUI_App.swift
+//  ColorizedApp-with-SwiftUI-
+//
+//  Created by OSIIOSdev on 23.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct ColorizedApp_with_SwiftUI_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
